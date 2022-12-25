2dd5f4619bb1dd2f07127258fdf71f3397fa15bf
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2022-12-25 17:50:58 +0100

    Implement clear(), strobe(), write() and other basic func

--- ws0010.c
+++ ws0010.c
@@ -1,22 +1,121 @@
 #include "ws0010.h"
 #include <stdint.h>
 
-static void write_cmd(uint8_t cmd)
+#define FIRST_LINE_HEAD_ADDR	((uint8_t)0x00)
+#define SECOND_LINE_HEAD_ADDR	((uint8_t)0x40)
+
+#define CHECK_BUSY_ATTEMPTS		200
+#define DELAY_BETWEEN_CHECKS_US 5000
+
+#define DISPLAY_COMMAND_MODE	0
+#define DISPLAY_DATA_MODE		1
+
+#define CLEAR_DISPLAY_CMD		((uint8_t)0x01)
+
+static void set_pins_to_write_cmd(ws0010_dev_t *dev)
 {
-	return;
+	dev->ll->reset_rs();
+	dev->ll->reset_rw();
 }
 
-static void write_data(uint8_t data)
+static void set_pins_to_write_data(ws0010_dev_t *dev)
 {
-	return;
+	dev->ll->set_rs();
+	dev->ll->reset_rw();
 }
 
-static int is_busy(void)
+/* wait until busy or return fail code: 1 */
+static int is_busy(ws0010_dev_t *dev)
 {
+	// int check_busy_cycles = 0;
+
+	// read DB7
+	// while (DB7 == 1)
+	// {
+	// 	++check_busy_cycles;
+	// 	if (check_busy_cycles >= CHECK_BUSY_ATTEMPTS) {
+	// 		return 1;
+	// 	}
+	// 	dev->ll->delay_us(DELAY_BETWEEN_CHECKS_US);
+	// 	read DB7	
+	// }
+
 	return 0;
 }
 
-ws0010_ret_t oled_init(ws0010_ll_t *ll_funcs)
+static int is_args_ok(ws0010_dev_t *dev)
+{
+	if (!dev || !dev->ll->delay_us || !dev->ll->reset_e ||
+		!dev->ll->reset_rs || !dev->ll->reset_rw ||
+		!dev->ll->set_bits_to_out_pins || !dev->ll->set_e ||
+		!dev->ll->set_rs || !dev->ll->set_rw) {
+		return 0;	
+	}
+
+	return 1;
+}
+
+static void pulse_strobe(ws0010_dev_t *dev)
+{
+	dev->ll->reset_e();
+	dev->ll->delay_us(1);
+
+	dev->ll->set_e();
+	dev->ll->delay_us(1);
+
+	dev->ll->reset_e();
+	dev->ll->delay_us(100);
+}
+
+static ws0010_ret_t write(ws0010_dev_t *dev, uint8_t byte, int mode)
+{
+	if (mode == DISPLAY_COMMAND_MODE) {
+		set_pins_to_write_cmd(dev);
+	} else if (mode == DISPLAY_DATA_MODE) {
+		set_pins_to_write_data(dev);
+	} else {
+		return WS0010_FAIL;
+	}
+
+	if (dev->bits == WS0010_8BITS) {
+		dev->ll->set_bits_to_out_pins(byte);
+		pulse_strobe(dev);
+	} else if (dev->bits == WS0010_4BITS) {
+		dev->ll->set_bits_to_out_pins(byte >> 4);
+		pulse_strobe(dev);
+
+		dev->ll->set_bits_to_out_pins(byte);
+		pulse_strobe(dev);
+	} else {
+		return WS0010_FAIL;
+	}
+
+	return WS0010_OK;
+}
+
+ws0010_ret_t ws0010_init(ws0010_dev_t *dev)
+{
+	if (!is_args_ok(dev)) {
+		return WS0010_FAIL;
+	}
+
+	/* wait for power stabilization */
+	dev->ll->delay_us(50000);
+
+	// function set
+
+	return WS0010_OK;
+}
+
+ws0010_ret_t ws0010_clear(ws0010_dev_t *dev)
+{
+	ws0010_ret_t ret = write(dev, CLEAR_DISPLAY_CMD, DISPLAY_COMMAND_MODE);
+	dev->ll->delay_us(7000);
+
+	return ret;
+}
+
+ws0010_ret_t ws0010_function_set(ws0010_dev_t *dev)
 {
 	return WS0010_OK;
 }
--- ws0010.h
+++ ws0010.h
@@ -7,6 +7,28 @@
 extern "C" {
 #endif
 
+/*
+ * Remember when power is turned ON,
+ * WS0010 is initialized automatically by an internal reset circuit
+ * to this state:
+ * 1. Display clear
+ *
+ * 2. Function set:
+ *     DL="1": 8-bit interface data
+ *     N="0": 1-line display
+ *     F="0": 5 x 8 dot character font
+ * 3. Power turn off
+ *     PWR=”0”
+ * 4. Display on/off control: D="0": Display off C="0": Cursor off B="0": Blinking off
+ * 5. Entry mode set
+ *     I/D="1": Increment by 1
+ *     S="0": No shift
+ * 6. Cursor/Display shift/Mode / Pwr
+ *     S/C=”0”, R/L=”1”: Shifts cursor position to the right
+ *     G/C=”0”: Character mode
+ *     Pwr=”1”: Internal DC-DC power on
+ **/
+
 /* Any functions of the lib returns one of these values */
 typedef enum
 {
@@ -14,13 +36,32 @@ typedef enum
 	WS0010_FAIL,
 } ws0010_ret_t;
 
+/* Character font size */
+typedef enum
+{
+	WS0010_5x8DOTS,
+	WS0010_5x10DOTS,
+} ws0010_dots_t;
+
+/* Interface data len */
+typedef enum
+{
+	WS0010_8BITS,
+	WS0010_4BITS,
+} ws0010_bits_t;
+
 /* Low Level functions which must be implemented by user */
 typedef struct
 {
 	/* us delay must be implemented fot strobe logic */
 	void (*delay_us)(uint32_t us);
 
-	/* control data pins */
+	/*
+	 * Control data pins.
+	 * Implement these func depending on display mode:
+	 *     8-bits - set all 8 bits to pins
+	 *     4-bits - set least significant bits (0-3) to pins
+	 **/
 	void (*set_bits_to_out_pins)(uint8_t value);
 
 	/* control RS pin */
@@ -38,11 +79,29 @@ typedef struct
 
 typedef struct
 {
-	/* data */
-} oled;
+	/* common count of lines in your display */
+	int line_count;
+
+	/* displayed symbols per one line */
+	int symbols_per_line;
+
+	/* take in account symbols not displayed on screen (for shifting) */
+	int max_symbols_per_line;
+
+	/* dots per characters */
+	ws0010_dots_t dots;
+
+	/* interface length */
+	ws0010_bits_t bits;
+
+	/* hardware dependent functions */
+	ws0010_ll_t *ll;
+} ws0010_dev_t;
 
 
-ws0010_ret_t oled_init(ws0010_ll_t *ll_funcs);
+ws0010_ret_t ws0010_init(ws0010_dev_t *dev);
+ws0010_ret_t ws0010_clear(ws0010_dev_t *dev);
+ws0010_ret_t ws0010_function_set(ws0010_dev_t *dev);
 
 #ifdef __cplusplus
 }
